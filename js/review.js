26697e628c6592d99202a29b8e4742d85eeeae36
Gabriela T <gtrujillo15@gmail.com>
2019-05-06 22:20:37 -0700

    adding items

--- js/review.js
+++ js/review.js
@@ -1,15 +1,37 @@
+var user = {}; //empty object
+var name = prompt('Enter name: ');
+console.log({ name });
+console.log(!!name);
+console.log(isNaN(name));
+
+// if (name === ' ' || name === '' || parseInt(name)) {
+//     alert('Enter Valid Name: ');
+// } else {
+//     user.name = name;
+// }
+
 excludedName = ['poopface', 'idiot'];
 var isExcluded = excludedName.find(function (excluded) {
     return excluded === name;
 });
 
-// arrow function
+
+//arrow function
 var newIsExcluded = excludedName.find((excluded) => excluded === name);
 
-if (name && isNaN(name) && !isExcluded) {
-    URLSearchParams.name = name;
+if (name && isNaN(name)) {
+    user.name = name;
     var hp = prompt('Enter your hp: ');
     console.log(!!hp);
-    console.log(typeof hp);
-    console.log(parseInt(hp));
+    console.error.log(typeof hp);
+    console.log(isNaN(parseInt(hp)));
+
+
+    if (hp && !isNaN(parseInt(hp))) {
+ user.power = parsInt(hp);
+    } else {
+        alert('Enter valid hp: ');
+    }
+} else {
+    alert('Enter Valid Name');
 }
